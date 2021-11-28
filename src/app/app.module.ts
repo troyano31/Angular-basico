1666d47e8284e3bbadc4e51482689390ebc2936c
Jheimer Correia <anthonycorreia61@gmail.com>
2021-11-28 10:34:48 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './componente/contador.component';
-import { HeroeComponent } from './Heroes/heroe/heroe.component';
-import { ListadoComponent } from './Heroes/listado/listado.component';
+
+
+import { HeroesModule } from './Heroes/heroes.module';
+import { ContadorModule } from './componente/contador.module';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
-    HeroeComponent,
-    ListadoComponent
+    
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
